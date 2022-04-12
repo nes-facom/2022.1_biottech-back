e05fd1ee5c5d42125a5dd30524efc1d7009436c0
LeonardoVileela <leovilela.empresa@gmail.com>
2022-04-11 23:56:30 -0400

    cors config

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -181,6 +181,9 @@ ServerRequest::addDetector('tablet', function ($request) {
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: *');
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
 
 
 /*
